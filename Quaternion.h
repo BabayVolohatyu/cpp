46a072e63cb5c8cff6d2df5030d045cc93293106
babayvolohatyu <babayvolohatyu@gmail.com>
2025-03-04 14:10:22 +0200

    Essentials

---
+++ Quaternion.h
@@ -0,0 +1,98 @@
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+#include <iostream>
+#include <cmath>
+
+class Quaternion {
+private:
+    double _w, _x, _y, _z;
+
+public:
+    Quaternion()
+        : _w(1.0), _x(0.0), _y(0.0), _z(0.0) {
+    }
+
+    Quaternion(double w, double x, double y, double z)
+        : _w(w), _x(x), _y(y), _z(z) {
+    }
+
+    Quaternion(double xRotation, double yRotation, double zRotation) {
+        double phi = degreesToRadians(xRotation);
+        double theta = degreesToRadians(yRotation);
+        double psi = degreesToRadians(zRotation);
+        _w = std::cos(phi / 2) * std::cos(theta / 2) * std::cos(psi / 2) +
+             std::sin(phi / 2) * std::sin(theta / 2) * std::sin(psi / 2);
+
+        _x = std::sin(phi / 2) * std::cos(theta / 2) * std::cos(psi / 2)
+             - std::cos(phi / 2) * std::sin(theta / 2) * std::sin(psi / 2);
+
+
+        _y = std::cos(phi / 2) * std::sin(theta / 2) * std::cos(psi / 2)
+             + std::sin(phi / 2) * std::cos(theta / 2) * std::sin(psi / 2);
+
+        _z = std::cos(phi / 2) * std::cos(theta / 2) * std::sin(psi / 2)
+             - std::sin(phi / 2) * std::sin(theta / 2) * std::cos(psi / 2);
+    }
+
+    ~Quaternion() = default;
+
+    static double degreesToRadians(double degrees) {
+        return degrees * M_PI / 180;
+    }
+
+    void normalize() {
+        double mag = magnitude();
+        _w /= mag;
+        _x /= mag;
+        _y /= mag;
+        _z /= mag;
+    }
+
+    Quaternion conjugate() const {
+        return Quaternion(_w, -_x, -_y, -_z);
+    }
+    double magnitude() const {
+        return std::sqrt(_w * _w + _x * _x + _y * _y + _z * _z);
+    }
+    Quaternion operator*(const Quaternion &other) const noexcept {
+        const double newW = _w * other._w - _x * other._x - _y * other._y - _z * other._z;
+        const double newX = _w * other._x + _x * other._w + _y * other._z - _z * other._y;
+        const double newY = _w * other._y - _x * other._z + _y * other._w + _z * other._x;
+        const double newZ = _w * other._z + _x * other._y - _y * other._x + _z * other._w;
+        return Quaternion(newW, newX, newY, newZ);
+    }
+
+    Quaternion& operator*=(const Quaternion &other) noexcept {
+        const double oldW = _w;
+        const double oldX = _x;
+        const double oldY = _y;
+        const double oldZ = _z;
+
+        _w = oldW * other._w - oldX * other._x - oldY * other._y - oldZ * other._z;
+        _x = oldW * other._x + oldX * other._w + oldY * other._z - oldZ * other._y;
+        _y = oldW * other._y - oldX * other._z + oldY * other._w + oldZ * other._x;
+        _z = oldW * other._z + oldX * other._y - oldY * other._x + oldZ * other._w;
+
+        return *this;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Quaternion &quaternion) {
+        os << quaternion._w << ' ' << quaternion._x << ' ' << quaternion._y << ' ' << quaternion._z;
+        return os;
+    }
+
+    friend std::istream &operator>>(std::istream &is, Quaternion &quaternion) = delete;
+
+    double w() { return _w; }
+    double x() { return _x; }
+    double y() { return _y; }
+    double z() { return _z; }
+
+    void setW(double w) { _w = w; }
+    void setX(double x) { _x = x; }
+    void setY(double y) { _y = y; }
+    void setZ(double z) { _z = z; }
+};
+
+#endif
---
+++ Vector3.h
@@ -0,0 +1,51 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include <iostream>
+
+class Vector3 {
+private:
+    double _x, _y, _z;
+
+public:
+    Vector3(): _x(0.0), _y(0.0), _z(0.0) {
+    }
+
+    Vector3(double x, double y, double z): _x(x), _y(y), _z(z) {
+    }
+
+    Vector3 operator+(const Vector3 &other) const {
+        return Vector3(_x + other._x, _y + other._y, _z + other._z);
+    }
+
+    Vector3 operator-(const Vector3& other) const {
+        return Vector3(_x - other._x, _y - other._y, _z - other._z);
+    }
+
+    Vector3 operator*(double scalar) const {
+      return Vector3(_x * scalar, _y * scalar, _z * scalar);
+    }
+
+    Vector3 operator/(double scalar) const {
+        return Vector3(_x / scalar, _y / scalar, _z / scalar);
+    }
+
+    void normalize(){
+      double mag = magnitude();
+      _x /= mag;
+      _y /= mag;
+      _z /= mag;
+    }
+
+    double magnitude() const {
+        return std::sqrt(_x * _x + _y * _y + _z * _z);
+    }
+    double x() const { return _x; }
+    double y() const { return _y; }
+    double z() const { return _z; }
+
+    void setX(double x) { _x = x; }
+    void setY(double y) { _y = y; }
+    void setZ(double z) { _z = z; }
+};
+#endif //VECTOR3_H
