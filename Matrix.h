651fca585dbf09d3e233c70b6a52ee3270edbb80
babayvolohatyu <babayvolohatyu@gmail.com>
2025-03-18 14:23:35 +0200

    Revert "add restriction for template to accept only numeric types"
    This reverts commit 855ed8355bdb20badc1cea94727e3f1a7bc260a6.

--- Matrix.h
+++ Matrix.h
@@ -1,7 +1,7 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-template <typename T, typename std::enable_if<std::is_arithmetic<T>::value, int>::type = 0>
+template<class T>
 class Matrix {
 private:
     std::vector<std::vector<T> > matrix;
